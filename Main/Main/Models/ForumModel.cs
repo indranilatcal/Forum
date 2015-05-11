18e97eb853c7d60d397f2ceb6a5513deb8f85ed1
indranilatcal <in201174>
2015-05-12 01:08:28 +0530

    Modified code to support auto registration to forum

---
+++ Main/Main/Models/ForumModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Main.Forum.Models
+{
+	public class MemberAddModel
+	{
+		public string UserName { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+
+		public bool IsApproved { get; set; }
+
+		public string Comment { get; set; }
+
+		public string SpamAnswer { get; set; }
+
+		public string SocialProfileImageUrl { get; set; }
+
+		public LoginType LoginType { get; set; }
+	}
+
+	public enum LoginType
+	{
+		Facebook,
+		Google,
+		Standard
+	}
+}
