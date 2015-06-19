7a0bfd96b3bad32b8a90ed2595ebb7ad8d4194cd
indranilatcal <in201174>
2015-06-19 20:53:45 +0530

    Added forumservice impl class

---
+++ Main/Main/Services/ForumService.cs
@@ -0,0 +1,63 @@
+﻿using Main.Forum.Models;
+using Main.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Main.Services
+{
+	public interface IForumService
+	{
+		Task<MembershipUser> GetUserAsync(string username);
+		Task RegisterAsync(MemberAddModel model);
+	}
+	public class ForumService : IForumService
+	{
+		private const string _forumBaseApiKey = "ForumBaseApi";
+		private static readonly Uri _baseUri;
+
+		static ForumService()
+		{
+			_baseUri = new Uri(ConfigurationManager.AppSettings[_forumBaseApiKey]);
+		}
+
+		public async Task<MembershipUser> GetUserAsync(string username)
+		{
+			using (var apiClient = GetClient())
+			{
+				MembershipUser user = null;
+				var response = await apiClient.GetAsync(string.Format("Registration/{0}", username));
+				if(response.IsSuccessStatusCode)
+					user = await response.Content.ReadAsAsync<MembershipUser>();
+
+				return user;
+			}
+		}
+
+
+		public async Task RegisterAsync(MemberAddModel member)
+		{
+			using (var apiClient = GetClient())
+			{
+				var response = await apiClient.PostAsJsonAsync("Registration", member);
+				response.EnsureSuccessStatusCode();
+			}
+		}
+
+		private static HttpClient GetClient()
+		{
+			var client = new HttpClient();
+
+			client.BaseAddress = _baseUri;
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			return client;
+		}
+	}
+}
