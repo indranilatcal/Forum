e32e17786b20d09961feb474280aec6d68ec45ed
indranilatcal <in201174>
2015-06-24 20:59:02 +0530

    Added missing LogService and Api 1st draft doc

---
+++ Main/Main/Services/LogService.cs
@@ -0,0 +1,39 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Main.Services
+{
+	public interface ILogger
+	{
+		void Info(string message);
+		void Error(string message);
+		void Error(string message, Exception exception);
+	}
+	public class LogService : ILogger
+	{
+		private static readonly Logger _logger;
+		static LogService()
+		{
+			_logger = LogManager.GetLogger("Default");
+		}
+
+		public void Error(string message, Exception exception)
+		{
+			_logger.Error(message, exception);
+		}
+
+		public void Info(string message)
+		{
+			_logger.Info(message);
+		}
+
+
+		public void Error(string message)
+		{
+			_logger.Error(message);
+		}
+	}
+}
